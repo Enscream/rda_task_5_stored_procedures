e2145f83928b4e3516302daa43d4e3ee32514def
Oleksandr Kirichenko <shvdw.on@gmail.com>
2026-05-23 12:02:18 +0300

    Task solved

--- task.sql
+++ task.sql
@@ -1,3 +1,45 @@
-USE ShopDB; 
+DROP DATABASE IF EXISTS ShopDB;
 
--- Create your stored procedure here
+CREATE DATABASE ShopDB;
+USE ShopDB;
+
+CREATE TABLE Countries (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+CREATE TABLE Products (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+CREATE TABLE Warehouses (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Address VARCHAR(100),
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID)
+);
+
+CREATE TABLE ProductInventory (
+    ID INT PRIMARY KEY,
+    ProductID INT,
+    WarehouseAmount INT,
+    WarehouseID INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID),
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID)
+);
+
+DELIMITER //
+
+CREATE PROCEDURE get_warehouse_product_inventory(IN warehouse_id INT)
+BEGIN
+    SELECT 
+        p.Name AS ProductName,
+        pi.WarehouseAmount
+    FROM ProductInventory pi
+    JOIN Products p ON pi.ProductID = p.ID
+    WHERE pi.WarehouseID = warehouse_id;
+END //
+
+DELIMITER ;
